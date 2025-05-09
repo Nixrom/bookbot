195fb127ac1b1160eb4e31a2b84caf0e3bfdb7d7
Nixrom <spm03green@gmail.com>
2025-05-09 17:03:48 -0400

    book_read_in_main

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def get_book_text(book):
+    with open(book) as f:
+        book_text = f.read()
+        return book_text
+
+def main(book_text):
+    print(book_text)
+
+main(get_book_text("./books/frankenstein.txt"))
+
