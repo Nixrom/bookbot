c066ed9630662812780b935242aa5ea536b72103
Nixrom <spm03green@gmail.com>
2025-05-09 19:27:43 -0400

    moved count function to stats

--- main.py
+++ main.py
@@ -1,20 +1,3 @@
-def get_book_text(book):
-    with open(book) as f:
-        book_text = f.read()
-        return book_text
+from stats import get_num_words
 
-def word_split(book_text):
-    words = book_text.split()
-    return words
-
-def word_count(words):
-    word_num = 0
-    for word in words:
-        word_num += 1
-    return word_num
-
-def main(book_text):
-    print(book_text)
-
-print(f"{word_count(word_split(get_book_text("./books/frankenstein.txt")))} words found in the document")
 
---
+++ stats.py
@@ -0,0 +1,20 @@
+def get_book_text(book):
+    with open(book) as f:
+        book_text = f.read()
+        return book_text
+
+def word_split(book_text):
+    words = book_text.split()
+    return words
+
+def word_count(words):
+    word_num = 0
+    for word in words:
+        word_num += 1
+    return word_num
+
+def main(book_text):
+    print(book_text)
+
+
+get_num_words = (f"{word_count(word_split(get_book_text("./books/frankenstein.txt")))} words found in the document")
