bd5f570dbb9495b9c6d0174dbc25dd8d4a800a19
Nixrom <spm03green@gmail.com>
2025-05-09 19:13:50 -0400

    words count function

--- main.py
+++ main.py
@@ -3,8 +3,18 @@ def get_book_text(book):
         book_text = f.read()
         return book_text
 
+def word_split(book_text):
+    words = book_text.split()
+    return words
+
+def word_count(words):
+    word_num = 0
+    for word in words:
+        word_num += 1
+    return word_num
+
 def main(book_text):
     print(book_text)
 
-main(get_book_text("./books/frankenstein.txt"))
+print(f"{word_count(word_split(get_book_text("./books/frankenstein.txt")))} words found in the document")
 
